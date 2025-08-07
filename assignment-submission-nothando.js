f74270dab9e9a1e61c914db74d6b55901e4491dc
Nothandomasiya <tadiewaishe@gmail.com>
2025-08-07 17:32:10 +0200

    assignment one

---
+++ assignment-submission-nothando.js
@@ -0,0 +1,7 @@
+//declaring two variables that is firstName and lastName and assigning two variables that is Nothando and Masiya
+const firstName="Nothando";
+const lastName="Masiya";
+// creating a third variable named fullName that combines firstName and lastName
+const fullName= firstName + " "+ lastName;
+//log the fullName to the console
+console.log(fullName)
